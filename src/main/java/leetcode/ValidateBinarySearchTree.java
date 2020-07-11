7485d228068a045f7cbf11a9a0dfe130ef5f749f
Kartik Gola <kartik.gola@birdeye.com>
2020-07-11 19:44:16 +0530

    Adding ValidateBinarySearchTree

---
+++ src/main/java/leetcode/ValidateBinarySearchTree.java
@@ -0,0 +1,25 @@
+/*
+ * Author: Kartik Gola
+ * Date: 11/07/20, 7:43 PM
+ * Copyright (c) 2020 | https://rattl.io
+ */
+
+package leetcode;
+
+import commons.TreeNode;
+
+public class ValidateBinarySearchTree {
+
+    private boolean valid(TreeNode root, long min, long max) {
+        if ( root != null ) {
+            if ( root.val > min && root.val < max )
+                return valid(root.left, min, root.val) && valid(root.right, root.val, max);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
