9f9508328a9a9e89dcd13676d06c0f3577555f5a
Kartik Gola <kartik.gola@birdeye.com>
2020-07-24 19:46:22 +0530

    Adding HeightOfBinaryTreeWithConnectedLeafNodes

---
+++ src/main/java/geeksforgeeks/HeightOfBinaryTreeWithConnectedLeafNodes.java
@@ -0,0 +1,28 @@
+/*
+ * Author: Kartik Gola
+ * Date: 24/07/20, 5:44 PM
+ * Copyright (c) 2020 | https://rattl.io
+ * Problem URL: https://www.geeksforgeeks.org/find-height-of-a-special-binary-tree-whose-leaf-nodes-are-connected/
+ */
+
+package geeksforgeeks;
+
+import ds.TreeNode;
+
+public class HeightOfBinaryTreeWithConnectedLeafNodes {
+
+    public int height(TreeNode root) {
+        if ( root != null ) {
+            int left = 0,
+                right = 0;
+            if ( root.left != null && root.left.right != root ) {
+                left = height(root.left);
+            }
+            if ( root.right != null && root.right.left != root ) {
+                right = height(root.right);
+            }
+            return 1 + Math.max(left, right);
+        }
+        return 0;
+    }
+}
