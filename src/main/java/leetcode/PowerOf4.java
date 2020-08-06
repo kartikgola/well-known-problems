d8bd4b4ddc2e34847dcc218e6ce2f1d221b63e2b
Kartik Gola <kartik.gola@birdeye.com>
2020-08-06 18:49:01 +0530

    Adding solution for PowerOf4

---
+++ src/main/java/leetcode/PowerOf4.java
@@ -0,0 +1,20 @@
+/*
+ * Author: Kartik Gola
+ * Date: 04/08/20, 7:44 PM
+ * Copyright (c) 2020 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/power-of-four/
+ */
+
+package leetcode;
+
+public class PowerOf4 {
+
+    public boolean isPowerOfFour(int num) {
+        return num > 0
+                // Should be power of 2
+                && (num & (num - 1)) == 0
+                // Should have a '1' placed at any even position like 0, 2, 4, etc.
+                && (num & 0x55555555) != 0;
+    }
+
+}
