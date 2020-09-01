9f5aeb15ff8a78a67b92975524ba15ab6c8d5a79
Kartik Gola <krtkgla@gmail.com>
2020-09-01 21:47:20 +0530

    Adding codechef solutions

---
+++ src/main/java/codechef/AUG20B/CHEFWARS.java
@@ -0,0 +1,33 @@
+/*
+ * Author: Kartik Gola
+ * Date: 8/13/20 9:40 PM
+ * Copyright (c) 2020 | https://rattl.io
+ * Problem URL: https://www.codechef.com/AUG20B/problems/CHEFWARS
+ */
+
+package codechef.AUG20B;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CHEFWARS {
+
+    public static void main (String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while ( t-- > 0 ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            while ( h > 0 && p > 0 ) {
+                h = h - p;
+                p = p / 2;
+            }
+            if ( h <= 0 )
+                System.out.println(1);
+            else
+                System.out.println(0);
+        }
+    }
+}
---
+++ src/main/java/codechef/AUG20B/SKMP.java
@@ -0,0 +1,54 @@
+/*
+ * Author: Kartik Gola
+ * Date: 8/13/20 11:06 PM
+ * Copyright (c) 2020 | https://rattl.io
+ * Problem URL:
+ */
+
+package codechef.AUG20B;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SKMP {
+
+    public static void main (String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while ( t-- > 0 ) {
+            String s = br.readLine();
+            String p = br.readLine();
+
+            StringBuilder ans = new StringBuilder();
+            int[] sMap = new int[26];
+            int[] pMap = new int[26];
+
+            // ccad, cad
+            // cadc < ccad
+
+            for ( char ch : s.toCharArray() )
+                sMap[ch - 'a']++;
+
+            for ( char ch : p.toCharArray() )
+                pMap[ch - 'a']++;
+
+            for ( int i = 0; i < sMap.length; ++i ) {
+                while ( (i + 'a') <= p.charAt(0) && sMap[i] > pMap[i] ) {
+                    ans.append((char)(i + 'a'));
+                    sMap[i]--;
+                }
+                if ( pMap[i] > 0 )
+                    sMap[i] -= pMap[i];
+            }
+            ans.append(p);
+            for ( int i = 0; i < sMap.length; ++i ) {
+                while ( sMap[i] > 0 ) {
+                    ans.append((char)(i + 'a'));
+                    sMap[i]--;
+                }
+            }
+
+            System.out.println(ans.toString());
+        }
+    }
+}
