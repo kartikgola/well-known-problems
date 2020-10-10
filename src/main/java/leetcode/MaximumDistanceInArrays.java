2ad4855c3bda441460895d477c352c5784f9f873
Kartik Gola <krtkgla@gmail.com>
2020-10-10 11:43:26 +0530

    Adding MaximumDistanceInArrays

--- src/main/java/leetcode/MaximumDistanceInArrays.java
+++ src/main/java/leetcode/MaximumDistanceInArrays.java
@@ -12,19 +12,22 @@ import java.util.List;
 public class MaximumDistanceInArrays {
 
     public int maxDistance(List<List<Integer>> arr) {
-        int dist = 0,
-            min = arr.get(0).get(0),
-            max = arr.get(0).get(arr.get(0).size() - 1);
+        int maxDist = 0,
+            minVal = arr.get(0).get(0),
+            maxVal = arr.get(0).get(arr.get(0).size() - 1);
 
         for ( int i = 1; i < arr.size(); ++i ) {
-            dist = Math.max(dist, Math.max(
-                Math.abs(max - arr.get(i).get(0)),
-                Math.abs(min - arr.get(i).get(arr.get(i).size() - 1))
+            final Integer currentMin = arr.get(i).get(0),
+                          currentMax = arr.get(i).get(arr.get(i).size() - 1);
+            maxDist = Math.max(maxDist, Math.max(
+                Math.abs(maxVal - currentMin),
+                Math.abs(currentMax - minVal)
             ));
-            min = Math.min(min, arr.get(i).get(0));
-            max = Math.max(max, arr.get(i).get(arr.get(i).size() - 1));
+            // Values of min/max may be from same array but dist is always updated by taking values from current array
+            minVal = Math.min(minVal, currentMin);
+            maxVal = Math.max(maxVal, currentMax);
         }
 
-        return dist;
+        return maxDist;
     }
 }
