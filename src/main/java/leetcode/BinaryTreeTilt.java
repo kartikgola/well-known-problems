4f292c1e8e24bb8090ab155d322401a7ca9251d4
Kartik Gola <krtkgla@gmail.com>
2020-11-08 18:33:24 +0530

    Adding BinaryTreeTilt and FindTheSmallestDivisorGivenAThreshold

---
+++ src/main/java/leetcode/BinaryTreeTilt.java
@@ -0,0 +1,30 @@
+/*
+ * Author: Kartik Gola
+ * Date: 11/8/20, 6:22 PM
+ * Copyright (c) 2020 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/binary-tree-tilt/
+ */
+
+package leetcode;
+
+import ds.TreeNode;
+
+public class BinaryTreeTilt {
+
+    int tilt = 0;
+
+    private int subtreeSumOf(TreeNode root) {
+        if ( root != null ) {
+            final int left = subtreeSumOf(root.left);
+            final int right = subtreeSumOf(root.right);
+            tilt += Math.abs(left - right);
+            return root.val + left + right;
+        }
+        return 0;
+    }
+
+    public int findTilt(TreeNode root) {
+        subtreeSumOf(root);
+        return tilt;
+    }
+}
