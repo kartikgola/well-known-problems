5a577d268b59747fb5aa57871a71dc9e14d7b469
Kartik Gola <krtkgla@gmail.com>
2020-12-17 20:32:51 +0530

    Refactoring code in ValidateBinarySearchTree

--- src/main/java/leetcode/ValidateBinarySearchTree.java
+++ src/main/java/leetcode/ValidateBinarySearchTree.java
@@ -2,6 +2,7 @@
  * Author: Kartik Gola
  * Date: 11/07/20, 7:43 PM
  * Copyright (c) 2020 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/validate-binary-search-tree/
  */
 
 package leetcode;
@@ -10,16 +11,17 @@ import ds.TreeNode;
 
 public class ValidateBinarySearchTree {
 
-    private boolean valid(TreeNode root, long min, long max) {
-        if ( root != null ) {
-            if ( root.val > min && root.val < max )
-                return valid(root.left, min, root.val) && valid(root.right, root.val, max);
-            return false;
+    private Boolean isValid(TreeNode root, Integer MIN_VALUE, Integer MAX_VALUE) {
+        if (root != null) {
+            return (MIN_VALUE == null || root.val > MIN_VALUE)
+                    && (MAX_VALUE == null || root.val < MAX_VALUE)
+                    && isValid(root.left, MIN_VALUE, root.val)
+                    && isValid(root.right, root.val, MAX_VALUE);
         }
         return true;
     }
 
     public boolean isValidBST(TreeNode root) {
-        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return isValid(root, null, null);
     }
 }
