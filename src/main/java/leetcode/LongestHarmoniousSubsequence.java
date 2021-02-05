20f15947e104d2a32de5123bbb1c1bc6f1d02ad5
Kartik Gola <krtkgla@gmail.com>
2021-02-05 12:32:00 +0530

    Adding LongestHarmoniousSubsequence

---
+++ src/main/java/leetcode/LongestHarmoniousSubsequence.java
@@ -0,0 +1,26 @@
+/*
+ * Author: Kartik Gola
+ * Date: 05/02/2021, 12:31
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/longest-harmonious-subsequence/
+ */
+
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestHarmoniousSubsequence {
+
+    public int findLHS(int[] nums) {
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+            ans = Math.max(ans,
+                    Math.max(map.getOrDefault(num - 1, -count), map.getOrDefault(num + 1, -count)) + count);
+        }
+        return ans;
+    }
+}
