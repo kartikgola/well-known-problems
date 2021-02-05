ef91562433562e7396cb05ea699c5307fe65258a
Kartik Gola <krtkgla@gmail.com>
2021-02-05 12:35:02 +0530

    Adding LinkedListCycle

---
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -0,0 +1,25 @@
+/*
+ * Author: Kartik Gola
+ * Date: 05/02/2021, 12:34
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/linked-list-cycle/
+ */
+
+package leetcode;
+
+import ds.ListNode;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        while (p1 != p2 && p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1 == p2;
+    }
+}
