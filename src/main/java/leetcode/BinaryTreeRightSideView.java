7b6ee8d958a8bb38341210763ea8242fdbf78642
Kartik Gola <krtkgla@gmail.com>
2021-02-06 18:15:45 +0530

    Adding BinaryTreeRightSideView

---
+++ src/main/java/leetcode/BinaryTreeRightSideView.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Kartik Gola
+ * Date: 2/6/21, 6:15 PM
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/binary-tree-right-side-view/
+ */
+
+package leetcode;
+
+import ds.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BinaryTreeRightSideView {
+    private Map<Integer, Integer> map = new LinkedHashMap<>();
+
+    private void dfs(TreeNode root, int level) {
+        if (root != null) {
+            map.put(level, root.val);
+            dfs(root.left, level+1);
+            dfs(root.right, level+1);
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        dfs(root, 0);
+        return new ArrayList<>(map.values());
+    }
+}
