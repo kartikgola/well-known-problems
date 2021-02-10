cafa47dcf64a18bc10550bc88361bf6dff05d760
Kartik Gola <krtkgla@gmail.com>
2021-02-10 20:25:40 +0530

    Adding EditDistance

---
+++ src/main/java/codechef/FEB21C/TEAMNAME.java
@@ -0,0 +1,23 @@
+/*
+ * Author: Kartik Gola
+ * Date: 10/02/2021, 14:20
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL: https://www.codechef.com/FEB21C/problems/TEAMNAME
+ */
+
+package codechef.FEB21C;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TEAMNAME {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(n);
+        }
+    }
+}
