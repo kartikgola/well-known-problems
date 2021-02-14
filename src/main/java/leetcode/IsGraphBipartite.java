327d014ec6a0156696bcfad5d4c8cfe0c0a0b905
Kartik Gola <krtkgla@gmail.com>
2021-02-14 18:44:23 +0530

    Updatin code in IsGraphBipartite

--- src/main/java/leetcode/IsGraphBipartite.java
+++ src/main/java/leetcode/IsGraphBipartite.java
@@ -2,34 +2,33 @@
  * Author: Kartik Gola
  * Date: 8/6/20 6:53 PM
  * Copyright (c) 2020 | https://rattl.io
- * Problem URL:
+ * Problem URL: https://leetcode.com/problems/is-graph-bipartite/
  */
 
 package leetcode;
 
 public class IsGraphBipartite {
 
-    private boolean dfs(int u, Boolean[] color, int[][] graph, boolean prevColor) {
-        color[u] = !prevColor;
-        for ( int v : graph[u] ) {
-            if ( color[v] == color[u] )
+    enum Color {
+        RED,
+        BLACK
+    }
+
+    private boolean dfs(int u, int[][] graph, Color[] colors, Color color) {
+        if (colors[u] != null)
+            return colors[u] == color;
+        colors[u] = color;
+        for (int v: graph[u])
+            if (!dfs(v, graph, colors, color == Color.RED ? Color.BLACK : Color.RED))
                 return false;
-            else if ( color[v] == null && !dfs(v, color, graph, color[u]) )
-                return false;
-        }
         return true;
     }
 
     public boolean isBipartite(int[][] graph) {
-        final int n = graph.length;
-        Boolean[] color = new Boolean[n];
-        for ( int i = 0; i < n; ++i ) {
-            if ( color[i] == null ) {
-                if ( !dfs(i, color, graph, false) )
-                    return false;
-            }
-        }
-
+        Color[] colors = new Color[graph.length];
+        for (int u = 0; u < graph.length; ++u)
+            if (colors[u] == null && !dfs(u, graph, colors, Color.RED))
+                return false;
         return true;
     }
 
