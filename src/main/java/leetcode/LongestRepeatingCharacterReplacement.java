a6063f7b8bcb576a90745734f1e1dcb6f7d9e9ed
Kartik Gola <krtkgla@gmail.com>
2021-03-13 20:18:04 +0530

    Updating LongestRepeatingCharacterReplacement

--- src/main/java/leetcode/LongestRepeatingCharacterReplacement.java
+++ src/main/java/leetcode/LongestRepeatingCharacterReplacement.java
@@ -15,32 +15,32 @@ public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
         final int n = s.length();
         Map<Character, Integer> map = new HashMap<>();
-        int left = 0, right = 0;
-        int maxLen = Integer.MIN_VALUE;
-        int maxVal = Integer.MIN_VALUE;
+        int begin = 0, end = 0;
+        int ans = Integer.MIN_VALUE;
+        int maxFreq = Integer.MIN_VALUE;
 
-        while ( right < n ) {
-            char ch = s.charAt(right);
+        while ( end < n ) {
+            char ch = s.charAt(end++);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-            maxVal = Math.max(maxVal, map.get(ch));
-            right++;
+            // Get the maximum frequency in the current window
+            maxFreq = Math.max(maxFreq, map.get(ch));
 
-            // Current window is invalid iff `window.size - maxValue > k`
-            // Example, if A:3, B:3, C:2 & k = 4 & left = 0, right = 8
-            // Longest string of length 8 can only be made for AAABBBC & not AAABBBCC
-            // Since we can do only 4 replacements at max & which will leave one 'C'
-            // Moreover, this while loop will only run once as window.size() will reduce by 1 due to left++
-            while ( right - left - maxVal > k ) {
-                ch = s.charAt(left);
+            // Current window has to be decreased only if `window.size - maxFreq > k`
+            // Example, given s = "AAABBBCC"
+            // if A:3, B:3, C:2 & k = 4 & left = 0, right = 8
+            // Since we can do only 4 replacements at max, for the substring "BBBC"
+            // Longest string of length 8 can only be made for `AAABBBC`
+            if (end - begin - maxFreq > k) {
+                ch = s.charAt(begin);
                 map.put(ch, map.get(ch) - 1);
-                if ( map.get(ch) == 0 )
+                if (map.get(ch) == 0)
                     map.remove(ch);
-                ++left;
+                ++begin;
             }
 
-            maxLen = Math.max(maxLen, right - left);
+            ans = Math.max(ans, end - begin);
         }
 
-        return maxLen == Integer.MIN_VALUE ? 0 : maxLen;
+        return ans == Integer.MIN_VALUE ? 0 : ans;
     }
 }
