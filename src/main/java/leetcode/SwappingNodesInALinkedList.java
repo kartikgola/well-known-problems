13a7267ac51660575217b222c0d7f94ef5b41389
Kartik Gola <krtkgla@gmail.com>
2021-03-20 13:23:04 +0530

    Adding Swapping

---
+++ src/main/java/leetcode/SwappingNodesInALinkedList.java
@@ -0,0 +1,36 @@
+/*
+ * Author: Kartik Gola
+ * Date: 3/20/21, 1:16 PM
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL:
+ */
+
+package leetcode;
+
+import ds.ListNode;
+import util.LinkedListUtils;
+
+public class SwappingNodesInALinkedList {
+
+    public ListNode swapNodes(ListNode head, int k) {
+        final int n = LinkedListUtils.findLength(head);
+        if (n <= 1)
+            return head;
+
+        ListNode begin = head;
+        for (int i = 0; i < k - 1; ++i) {
+            begin = begin.next;
+        }
+
+        ListNode end = head;
+        for (int i = 0; i < n - k; ++i) {
+            end = end.next;
+        }
+
+        int temp = begin.val;
+        begin.val = end.val;
+        end.val = temp;
+
+        return head;
+    }
+}
