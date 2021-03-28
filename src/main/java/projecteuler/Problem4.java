c129fddcd317fe644f0a28e72213f5f476dabe9e
Kartik Gola <krtkgla@gmail.com>
2021-03-29 00:17:14 +0530

    Updating Problem4

--- src/main/java/projecteuler/Problem4.java
+++ src/main/java/projecteuler/Problem4.java
@@ -7,8 +7,6 @@
 
 package projecteuler;
 
-import util.MathUtils;
-
 public class Problem4 {
 
     public int solve() {
@@ -16,9 +14,9 @@ public class Problem4 {
         for (int num1 = 999; num1 >= 100; --num1) {
             for (int num2 = 999; num2 >= 100; --num2) {
                 int prod = num1 * num2;
-                if (MathUtils.isPalindrome(prod) && prod > max) {
-                    max = prod;
-                }
+//                if (MathUtils.isPalindrome(prod) && prod > max) {
+//                    max = prod;
+//                }
             }
         }
         return max;
