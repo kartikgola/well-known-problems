609e05d8c5c72e8d2547ba15251d1a5d4888e957
Kartik Gola <krtkgla@gmail.com>
2021-06-08 19:57:12 +0530

    Adding LC solutions

---
+++ src/test/java/leetcode/TheMaze2Test.java
@@ -0,0 +1,30 @@
+/*
+ * Author: Kartik Gola
+ * Date: 6/8/21, 6:13 PM
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL:
+ */
+
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheMaze2Test {
+
+    @Test
+    void shortestDistance() {
+        new TheMaze2().shortestDistance(
+            new int[][]{
+                    {0,0,1,0,0},
+                    {0,0,0,0,0},
+                    {0,0,0,1,0},
+                    {1,1,0,1,1},
+                    {0,0,0,0,0}
+            },
+            new int[]{0, 4},
+            new int[]{4, 4}
+        );
+    }
+}
