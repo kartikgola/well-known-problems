dc1a7fc87b435c7172294ce6f210b018cd9a60d9
Kartik Gola <krtkgla@gmail.com>
2021-06-11 00:18:29 +0530

    Adding LC solutions

---
+++ src/main/java/leetcode/MyCalendar1.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Kartik Gola
+ * Date: 6/10/21, 8:02 PM
+ * Copyright (c) 2021 | https://rattl.io
+ * Problem URL: https://leetcode.com/problems/my-calendar-i/
+ */
+
+package leetcode;
+
+import java.util.TreeMap;
+
+public class MyCalendar1 {
+
+    private TreeMap<Integer, Integer> map = new TreeMap<>();
+
+    public MyCalendar1() { }
+
+    public boolean book(int start, int end) {
+        Integer left = map.floorKey(start);
+        Integer right = map.ceilingKey(start);
+        if ((left == null || map.get(left) <= start) && (right == null || right >= end)) {
+            map.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
