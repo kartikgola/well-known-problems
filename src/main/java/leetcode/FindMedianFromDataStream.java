3e4c2cea32c9a771e7a87ad1a3265d76750a21e2
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-07-12 13:32:12 +0530

    Adding solution for FindMedianFromDataStream

---
+++ src/main/java/leetcode/FindMedianFromDataStream.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Kartik Gola
+ * Date: 7/12/21, 9:57 AM
+ * Copyright (c) 2021 | https://kartikgola.com
+ * Problem URL: https://leetcode.com/problems/find-median-from-data-stream/
+ */
+
+package leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class FindMedianFromDataStream {
+
+    private Queue<Integer> lows = new PriorityQueue<>((a, b) -> b - a);
+    private Queue<Integer> highs = new PriorityQueue<>();
+
+    /** initialize your data structure here. */
+    public FindMedianFromDataStream() {
+    }
+
+    public void addNum(int num) {
+        highs.add(num);
+        lows.add(highs.poll());
+        if (lows.size() > highs.size())
+            highs.add(lows.poll());
+    }
+
+    public double findMedian() {
+        return highs.size() > lows.size() ? highs.peek() : (highs.peek() + lows.peek()) / 2.0;
+    }
+}
