4817a8f57099d5152d180d3fab08178fac54da84
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-08-12 18:08:38 +0530

    Adding sol'n for LC HouseRobber & HouseRobber2

--- src/main/java/practice/leetcode/HouseRobber.java
+++ src/main/java/practice/leetcode/HouseRobber.java
@@ -9,7 +9,15 @@ package practice.leetcode;
 
 public class HouseRobber {
 
+    private int rob(int[] nums, int i, Integer[] dp) {
+        if (i >= nums.length)
+            return 0;
+        if (dp[i] != null)
+            return dp[i];
+        return dp[i] = Math.max(nums[i] + rob(nums, i+2, dp), rob(nums, i+1, dp));
+    }
+
     public int rob(int[] nums) {
-        return -1;
+        return rob(nums, 0, new Integer[nums.length]);
     }
 }
--- src/main/java/practice/leetcode/HouseRobber2.java
+++ src/main/java/practice/leetcode/HouseRobber2.java
@@ -9,7 +9,18 @@ package practice.leetcode;
 
 public class HouseRobber2 {
 
+    private int rob(int[] nums, int i, int end, Integer[] dp) {
+        if (i >= end)
+            return 0;
+        if (dp[i] != null)
+            return dp[i];
+        return dp[i] = Math.max(nums[i] + rob(nums, i+2, end, dp), rob(nums, i+1, end, dp));
+    }
+
     public int rob(int[] nums) {
-        return -1;
+        final int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        return Math.max(rob(nums, 0, n-1, new Integer[n]), rob(nums, 1, n, new Integer[n]));
     }
 }
