54f5f41d23bf9c2bda7799d466f03125571b7985
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-08-27 00:25:43 +0530

    Adding LC solutions

---
+++ src/main/java/practice/leetcode/SumOfSquareNumbers.java
@@ -0,0 +1,52 @@
+/*
+ * Author: Kartik Gola
+ * Date: 8/25/21, 10:10 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+public class SumOfSquareNumbers {
+
+    public boolean judgeSquareSum(int c) {
+        // smallest number possible among a or b
+        long l = 0;
+        // largest number bounded to floor(sqrt(c)) since both the numbers must be LTE c
+        long r = (long)Math.floor(Math.sqrt(c));
+
+        while (l <= r) {
+            long s = l*l+r*r;
+            if (s == c)
+                return true;
+            else if (s > c)
+                r--;
+            else
+                l++;
+        }
+
+        return false;
+    }
+
+    public boolean judgeSquareSumFermatTheorem(int c) {
+        // 'c' can be expressed as sum of squares of 2 positive nos. only if
+        // in prime factorization of 'c', every prime of the form 4k+3
+        // occurs even no. of times
+        for (int i = 2; i <= Math.sqrt(c); i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        // If it is a prime, it should not be of the form 4k+3 (since it will occur odd no. of times, i.e. 1)
+        return c % 4 != 3;
+    }
+
+    public boolean judgeSquareSumBinarySearch(int c) {
+        return true;
+    }
+}
---
+++ src/main/java/practice/leetcode/VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,83 @@
+/*
+ * Author: Kartik Gola
+ * Date: 8/26/21, 11:06 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class VerifyPreorderSerializationOfABinaryTree {
+
+    public boolean isValidSerialization(String preorder) {
+        if (preorder.equals("#"))
+            return true;
+
+        Stack<String> sk = new Stack<>();
+        StringTokenizer st = new StringTokenizer(preorder, ",");
+        String root = null;
+
+        while (st.hasMoreTokens()) {
+            String tk = st.nextToken();
+            if (tk.equals("#")) {
+                if (sk.isEmpty())
+                    return false;
+                // [..,4,L becomes [..,4,L,R
+                else if (sk.peek().equals("L"))
+                    sk.push("R");
+                else
+                    // [..,4,# becomes [..,4,L
+                    sk.push("L");
+            } else {
+                if (sk.isEmpty()) {
+                    if (root == null) {
+                        // set the root node
+                        root = tk;
+                    } else {
+                        // there can't be 2 roots!
+                        return false;
+                    }
+                }
+                sk.push(tk);
+            }
+
+            // Recursively resolve all series of the form [..,4,L,R
+            while (sk.size() >= 3 && sk.peek().equals("R")) {
+                String right = sk.pop();
+                String left = sk.pop();
+                String head = sk.pop();
+                if (right.equals("R") && left.equals("L") && !head.equals("L") && !head.equals("R")) {
+                    if (!sk.isEmpty()) {
+                        if (sk.peek().equals("L")) {
+                            sk.push("R");
+                        } else {
+                            sk.push("L");
+                        }
+                    }
+                } else {
+                    sk.push(head);
+                    sk.push(left);
+                    sk.push(right);
+                }
+            }
+        }
+
+        return sk.isEmpty();
+    }
+
+    // https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/discuss/78551/7-lines-Easy-Java-Solution
+    public boolean isValidSerializationDegrees(String preorder) {
+        // diff = outdegree - indegree
+        int diff = 1;
+        for (StringTokenizer st = new StringTokenizer(preorder, ","); st.hasMoreTokens();) {
+            String tkn = st.nextToken();
+            if (--diff < 0)
+                return false;
+            if (!tkn.equals("#"))
+                diff += 2;
+        }
+        return diff == 0;
+    }
+}
