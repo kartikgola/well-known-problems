ce4ef203dd17df980c60861a4f87cea343c1a5b8
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-09-10 20:24:19 +0530

    Adding ReconstructItinerary

---
+++ src/main/java/practice/leetcode/ReconstructItinerary.java
@@ -0,0 +1,41 @@
+/*
+ * Author: Kartik Gola
+ * Date: 9/10/21, 1:52 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import java.util.*;
+
+public class ReconstructItinerary {
+
+    private void dfs(String from, Map<String, Queue<String>> map, List<String> res) {
+        Queue<String> pq = map.getOrDefault(from, new PriorityQueue<>());
+        if (pq.isEmpty()) {
+            res.add(from);
+            return;
+        }
+        while (!pq.isEmpty()) {
+            String to = pq.poll();
+            dfs(to, map, res);
+        }
+        res.add( from );
+    }
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, Queue<String>> map = new HashMap<>();
+        for ( List<String> tkt : tickets ) {
+            String from = tkt.get(0);
+            String to = tkt.get(1);
+            map.putIfAbsent(from, new PriorityQueue<>());
+            map.get(from).add(to);
+        }
+
+        List<String> res = new ArrayList<>();
+        dfs( "JFK", map, res );
+
+        Collections.reverse(res);
+        return res;
+    }
+}
