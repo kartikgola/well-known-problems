a31d5508f612de391948723865d88de23616f378
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-09-11 15:09:51 +0530

    Adding CompareStringsByFrequencyOfSmallestCharacter

---
+++ src/main/java/practice/leetcode/CompareStringsByFrequencyOfSmallestCharacter.java
@@ -0,0 +1,48 @@
+/*
+ * Author: Kartik Gola
+ * Date: 9/11/21, 12:13 AM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import util.ArrayUtils;
+
+import java.util.Arrays;
+
+public class CompareStringsByFrequencyOfSmallestCharacter {
+
+    private int f(String s) {
+        int[] map = new int[26];
+        for (char ch: s.toCharArray())
+            map[ch-'a']++;
+        for (int val: map)
+            if (val > 0)
+                return val;
+        return 0;
+    }
+
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] ans = new int[queries.length];
+        int[] fw = new int[words.length];
+
+        for (int i = 0; i < words.length; ++i)
+            fw[i] = f(words[i]);
+
+        Arrays.sort(fw);
+
+        for (int i = 0; i < queries.length; ++i) {
+            int k = f(queries[i]);
+            int j = ArrayUtils.Bisect.bisectRight(fw, 0, fw.length, k);
+            if (j < fw.length) {
+                if (fw[j] > k)
+                    ans[i] = fw.length-j;
+                else
+                    ans[i] = fw.length-1-j;
+            }
+        }
+
+        return ans;
+    }
+
+}
