0717f1ade1a86f322f3843da9d3a7c449478cb75
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-09-13 22:34:23 +0530

    Updating comment

--- src/main/java/practice/leetcode/PathWithMinimumEffort.java
+++ src/main/java/practice/leetcode/PathWithMinimumEffort.java
@@ -7,10 +7,7 @@
 
 package practice.leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class PathWithMinimumEffort {
 
@@ -49,7 +46,32 @@ public class PathWithMinimumEffort {
         return effort[m-1][n-1];
     }
 
-    public int minimumEffortPathBSpfa(int[][] heights) {
-        return -1;
+    public int minimumEffortPathSpfa(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        int[][] effort = new int[m][n];
+        for (int[] e: effort)
+            Arrays.fill(e, Integer.MAX_VALUE);
+        effort[0][0] = 0;
+
+        Queue<int[]> q = new LinkedList<>();
+        int[][] po = new int[][]{{-1,0}, {0,1}, {1,0}, {0,-1}};
+        // tuple of (effort, i, j)
+        q.add(new int[]{0, 0, 0});
+
+        while (!q.isEmpty()) {
+            int[] c = q.poll();
+            for (int[] p: po) {
+                int x = c[1]+p[0];
+                int y = c[2]+p[1];
+                if (x >= 0 && x < m && y >= 0 && y < n) {
+                    if (Math.max(c[0], Math.abs(heights[x][y] - heights[c[1]][c[2]])) < effort[x][y]) {
+                        effort[x][y] = Math.max(c[0], Math.abs(heights[x][y] - heights[c[1]][c[2]]));
+                        q.add(new int[]{effort[x][y], x, y});
+                    }
+                }
+            }
+        }
+        return effort[m-1][n-1];
     }
 }
