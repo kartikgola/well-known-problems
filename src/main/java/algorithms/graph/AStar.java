45681e44e45d0bdd6e3e61486db988782325f99c
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-10-17 13:18:51 +0530

    Adding BestTimeToBuyAndSellStock3

---
+++ src/main/java/algorithms/graph/AStar.java
@@ -0,0 +1,17 @@
+/*
+ * Author: Kartik Gola
+ * Date: 10/16/21, 5:02 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package algorithms.graph;
+
+import util.ds.graph.Graph;
+import java.util.Map;
+
+public class AStar<T extends Number> {
+
+    public Map<T, Integer> dijkstra(Graph<T> graph, T source) {
+
+    }
+}
--- src/main/java/practice/leetcode/BestTimeToBuyAndSellStock3.java
+++ src/main/java/practice/leetcode/BestTimeToBuyAndSellStock3.java
@@ -9,7 +9,33 @@ package practice.leetcode;
 
 public class BestTimeToBuyAndSellStock3 {
 
-//    public int maxProfit(int[] prices) {
-//
-//    }
+    public int maxProfit(int[] prices) {
+        final int n = prices.length;
+        int prev = 0;
+
+        // leftProfit[i] = max profit for 1 transaction in prices[0..i]
+        int[] leftProfit = new int[n];
+        for (int i = 1; i < n; ++i) {
+            leftProfit[i] = Math.max(leftProfit[i-1], prices[i]-prices[prev]);
+            if (prices[prev] > prices[i]) {
+                prev = i;
+            }
+        }
+
+        prev = n-1;
+        // rightProfit[i] = max profit for 1 transaction in prices[i..n-1]
+        int[] rightProfit = new int[n];
+        for (int i = n-2; i >= 0; --i) {
+            rightProfit[i] = Math.max(rightProfit[i+1], prices[prev]-prices[i]);
+            if (prices[prev] < prices[i])
+                prev = i;
+        }
+
+        int ans = Math.max(leftProfit[n-1], rightProfit[0]);
+        for (int i = 0; i < n-1; ++i) {
+            ans = Math.max(ans, leftProfit[i] + rightProfit[i+1]);
+        }
+
+        return ans;
+    }
 }
