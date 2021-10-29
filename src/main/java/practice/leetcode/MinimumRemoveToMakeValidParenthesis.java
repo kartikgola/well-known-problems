4975e12f1c491051c8011f19cbdf864f6cf457f0
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-10-29 22:36:51 +0530

    Adding MinimumRemoveToMakeValidParenthesis

---
+++ src/main/java/practice/leetcode/MinimumRemoveToMakeValidParenthesis.java
@@ -0,0 +1,42 @@
+/*
+ * Author: Kartik Gola
+ * Date: 10/29/21, 10:36 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import java.util.Stack;
+
+public class MinimumRemoveToMakeValidParenthesis {
+
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> st = new Stack<>();
+        // decides whether a character will be kept in ans or not
+        boolean[] keep = new boolean[s.length()];
+
+        // We keep all non-parenthesis chars
+        // We keep all matching (, ) pairs
+        // Others are by default not kept (default false)
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!st.isEmpty()) {
+                    keep[st.pop()] = true;
+                    keep[i] = true;
+                }
+            } else {
+                keep[i] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < keep.length; ++i) {
+            if (keep[i])
+                sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
