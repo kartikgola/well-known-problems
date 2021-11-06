4f2d7feda2b05c5946d019a161155e9181e362fc
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-11-06 23:58:34 +0530

    Adding SingleNumber2

---
+++ src/main/java/practice/leetcode/SingleNumber2.java
@@ -0,0 +1,34 @@
+/*
+ * Author: Kartik Gola
+ * Date: 11/6/21, 11:45 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+public class SingleNumber2 {
+
+    // O(n) time and O(1) space solution
+    // Explanation - https://leetcode.com/problems/single-number-ii/solution/
+    public int singleNumber(int[] nums) {
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num : nums) {
+            // first appearence:
+            // add num to seen_once
+            // don't add to seen_twice because of presence in seen_once
+
+            // second appearance:
+            // remove num from seen_once
+            // add num to seen_twice
+
+            // third appearance:
+            // don't add to seen_once because of presence in seen_twice
+            // remove num from seen_twice
+            seenOnce = ~seenTwice & (seenOnce ^ num);
+            seenTwice = ~seenOnce & (seenTwice ^ num);
+        }
+
+        return seenOnce;
+    }
+}
