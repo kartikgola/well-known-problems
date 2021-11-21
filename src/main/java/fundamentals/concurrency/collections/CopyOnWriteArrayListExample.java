b8ef15e87f52a2180980d94cbd7e0b41d73bb5a1
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-11-21 17:16:34 +0530

    Adding CopyOnWriteArrayListExample

--- src/main/java/fundamentals/concurrency/collections/CopyOnWriteArrayListExample.java
+++ src/main/java/fundamentals/concurrency/collections/CopyOnWriteArrayListExample.java
@@ -12,7 +12,7 @@ import java.util.concurrent.*;
 
 public class CopyOnWriteArrayListExample {
 
-    public static void copyOnWriteArrayListExample() {
+    public static void copyOnWriteArrayListExample() throws InterruptedException {
         ExecutorService es = Executors.newFixedThreadPool(2);
         try {
             // CopyOnWriteArrayList will copy the entire list internally
@@ -20,30 +20,14 @@ public class CopyOnWriteArrayListExample {
             // This is LIKE an Immutable List with the difference that the old reference is kept.
             List<Integer> list = new CopyOnWriteArrayList<>(Arrays.asList(1,2,3));
 
-            // Example 1
             for(Integer item: list) {
                 System.out.print(item + " ");
                 // Adding elements to list will not affect the current iterator of 'list'
                 list.add(4);
             }
-            System.out.println();
+
             // However, the 'list' currently contains [1,2,3,4,4,4]
-            System.out.println(list);
-
-
-            // Example 2
-            List<Integer> list2 = new CopyOnWriteArrayList<>();
-            es.execute(() -> {
-                // Keeps modifying the list
-                for (int elem = 1; elem <= 1000_000; ++elem)
-                    list2.add(elem);
-            });
-            es.execute(() -> {
-                // Gets a list iterator and prints lis2.size()
-                for (Integer val: list2);
-                System.out.println(list.size());
-            });
-
+            System.out.println("\n" + list);
         } catch (Exception exception) {
             exception.printStackTrace();
         } finally {
@@ -51,7 +35,7 @@ public class CopyOnWriteArrayListExample {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         copyOnWriteArrayListExample();
     }
 }
