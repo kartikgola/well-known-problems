02e04c1a14685cb58cba6df031fb228e770eca6b
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-11-25 23:47:52 +0530

    Adding IntervalListIntersection

--- src/main/java/practice/leetcode/IntervalListIntersection.java
+++ src/main/java/practice/leetcode/IntervalListIntersection.java
@@ -7,52 +7,48 @@
 package practice.leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class IntervalListIntersection {
 
-    private boolean isIntersecting(int[] a, int[] b) {
-        return a[1] >= b[0] && b[1] >= a[0];
-    }
-
-    private int[] getIntersection(int[] a, int[] b) {
-        return new int[]{ Math.max(a[0], b[0]), Math.min(a[1], b[1]) };
-    }
-
-    private boolean isSmaller(int[] a, int[] b) {
-        return a[1] <= b[1];
+    private int[] intersectionOf(int[] x, int[] y) {
+        if (x[1] < y[0] || x[0] > y[1])
+            return new int[]{};
+        return new int[]{Math.max(x[0], y[0]), Math.min(x[1], y[1])};
     }
 
     public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> al = new ArrayList<>();
         final int m = A.length;
         final int n = B.length;
-        List<List<Integer>> res = new ArrayList<>(m);
-
         int i = 0, j = 0;
-        while ( i < m && j < n ) {
-            if ( isIntersecting(A[i], B[j]) ) {
-                int[] temp = getIntersection(A[i], B[j]);
-                res.add( Arrays.asList( temp[0], temp[1] ) );
-            }
-            if ( isSmaller(A[i], B[j]) ) {
-                ++i;
+
+        while (i < m && j < n) {
+            int[] a = A[i];
+            int[] b = B[j];
+            int[] c = intersectionOf(a, b);
+            if (c.length == 0) {
+                if (a[1] > b[1])
+                    j++;
+                else
+                    i++;
             } else {
-                ++j;
+                al.add(c);
+                if (a[1] >= c[1]) {
+                    j++;
+                } else if (b[1] > c[1]) {
+                    i++;
+                } else {
+                    i++; j++;
+                }
             }
         }
 
-        if ( res.size() == 0 )
-            return new int[][]{};
+        int[][] ans = new int[al.size()][2];
+        for (int k = 0; k < ans.length; ++k)
+            ans[k] = al.get(k);
 
-        int w = 0;
-        int[][] resArr = new int[res.size()][2];
-        for ( List<Integer> r : res ) {
-            resArr[ w ][0] = r.get(0);
-            resArr[ w++ ][1] = r.get(1);
-        }
-
-        return resArr;
+        return ans;
     }
 
 }
