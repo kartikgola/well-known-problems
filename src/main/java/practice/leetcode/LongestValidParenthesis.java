f388316af5eaccb9b9a43dcf3f2427140afa6764
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-11-30 22:05:36 +0530

    Updatiing LongestValidParenthesis

--- src/main/java/practice/leetcode/LongestValidParenthesis.java
+++ src/main/java/practice/leetcode/LongestValidParenthesis.java
@@ -15,9 +15,11 @@ public class LongestValidParenthesis {
             if (s.charAt(i) == ')') {
                 if (s.charAt(i-1) == '(') {
                     // x()
+                    // --i
                     dp[i] = (i-2 >= 0 ? dp[i-2] : 0) + 2;
                 } else if (i-dp[i-1]-1 >= 0 && s.charAt(i-dp[i-1]-1) == '(') {
                     // x(())
+                    // ----i
                     dp[i] = dp[i-1] + (i-dp[i-1]-2 >= 0 ? dp[i-dp[i-1]-2] : 0) + 2;
                 }
                 ans = Math.max(ans, dp[i]);
