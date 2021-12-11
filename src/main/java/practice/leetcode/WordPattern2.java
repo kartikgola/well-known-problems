afa5e3e5bc422c844abdfdcc2ea0de43a5b04948
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-12-11 16:24:23 +0530

    Adding WordPattern2

---
+++ src/main/java/practice/leetcode/WordPattern2.java
@@ -0,0 +1,57 @@
+/*
+ * Author: Kartik Gola
+ * Date: 12/11/21, 4:17 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WordPattern2 {
+
+    private boolean isMatch(String p, int i, String s, int j, Map<Character, String> charMap, Set<String> used) {
+        // S and P both are processed
+        if (j == s.length())
+            return i == p.length();
+
+        // if pattern is all consumed, and S is still left unprocessed
+        if (i == p.length())
+            return false;
+
+        char ch = p.charAt(i);
+        if (charMap.containsKey(ch)) {
+            // ch is already mapped previously, so we check match from the same mapping
+            String val = charMap.get(ch);
+            if (j + val.length() > s.length())
+                return false;
+            for (int k = j, l = 0; l < val.length(); ++k, ++l) {
+                if (val.charAt(l) != s.charAt(k))
+                    return false;
+            }
+            return isMatch(p, i+1, s, j+val.length(), charMap, used);
+        } else {
+            for (int k = j; k < s.length(); ++k) {
+                String val = s.substring(j, k+1);
+                // since mapping is bijective, there should not be a case where same substring is mapped to 2 diff chars
+                if (!used.contains(val)) {
+                    used.add(val);
+                    charMap.put(ch, val);
+                    if (isMatch(p, i+1, s, k+1, charMap, used)) {
+                        return true;
+                    }
+                    used.remove(val);
+                    charMap.remove(ch);
+                }
+            }
+            return false;
+        }
+    }
+
+    public boolean wordPatternMatch(String pattern, String s) {
+        return isMatch(pattern, 0, s, 0, new HashMap<>(), new HashSet<>());
+    }
+}
