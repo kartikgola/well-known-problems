8aafae1adae2d45b5345a620c8db69514e8b448d
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-12-14 09:58:26 +0530

    Adding DeleteNodeInABST

---
+++ src/main/java/practice/leetcode/DeleteNodeInABST.java
@@ -0,0 +1,52 @@
+/*
+ * Author: Kartik Gola
+ * Date: 12/14/21, 9:58 AM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import util.ds.tree.binary.TreeNode;
+
+public class DeleteNodeInABST {
+
+    private TreeNode successor(TreeNode root) {
+        if (root != null) {
+            if (root.left != null)
+                return successor(root.left);
+            return root;
+        }
+        return null;
+    }
+
+    private TreeNode predecessor(TreeNode root) {
+        if (root != null) {
+            if (root.right != null)
+                return predecessor(root.right);
+            return root;
+        }
+        return null;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root != null) {
+            if (root.val == key) {
+                if (root.left == null && root.right == null) {
+                    return null;
+                } else if (root.right != null) {
+                    root.val = successor(root.right).val;
+                    root.right = deleteNode(root.right, root.val);
+                } else {
+                    root.val = predecessor(root.left).val;
+                    root.left = deleteNode(root.left, root.val);
+                }
+            } else if (root.val > key) {
+                root.left = deleteNode(root.left, key);
+            } else {
+                root.right = deleteNode(root.right, key);
+            }
+            return root;
+        }
+        return null;
+    }
+}
