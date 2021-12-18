c70f19b361a4dd001f4085a6d9a10cfbae91714e
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-12-18 18:59:26 +0530

    Adding RemoveAllAdjacentDuplicatesInAString2

---
+++ src/main/java/practice/leetcode/RemoveAllAdjacentDuplicatesInAString2.java
@@ -0,0 +1,50 @@
+/*
+ * Author: Kartik Gola
+ * Date: 12/18/21, 6:45 PM
+ * Copyright (c) 2021 | https://kartikgola.com
+ */
+
+package practice.leetcode;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInAString2 {
+
+    class Item {
+        char val;
+        int count;
+        Item(char val, int count) {
+            this.val = val;
+            this.count = count;
+        }
+        public String toString() {
+            return "(" + val + ", " + count + ")";
+        }
+    }
+
+    public String removeDuplicates(String s, int k) {
+        Stack<Item> stack = new Stack<>();
+        for (char ch: s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(new Item(ch, 1));
+            } else {
+                Item peek = stack.peek();
+                if (peek.val == ch) {
+                    peek.count++;
+                } else {
+                    stack.push(new Item(ch, 1));
+                }
+            }
+            if (!stack.isEmpty() && stack.peek().count == k)
+                stack.pop();
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while (!stack.isEmpty()) {
+            Item pop = stack.pop();
+            ans.append((""+pop.val).repeat(pop.count));
+        }
+
+        return ans.reverse().toString();
+    }
+}
