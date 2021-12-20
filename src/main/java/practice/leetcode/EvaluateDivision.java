69b178532597cfca0711e580086b145387447de8
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-12-20 21:17:02 +0530

    Adding EvaluateDivision

--- src/main/java/practice/leetcode/EvaluateDivision.java
+++ src/main/java/practice/leetcode/EvaluateDivision.java
@@ -6,9 +6,7 @@
 
 package practice.leetcode;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class EvaluateDivision {
 
@@ -56,6 +54,61 @@ public class EvaluateDivision {
                 ans[i] = -1.0;
             else
                 ans[i] = (double) dist.get(x1) / dist.get(x2);
+        }
+
+        return ans;
+    }
+
+    private class State {
+        String key;
+        double val;
+        State(String key, double val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    // Uses BFS to get from query[0] to query[1]
+    private double calc(List<String> query, Map<String, Map<String, Double>> map) {
+        Queue<State> q = new LinkedList<>();
+        String from = query.get(0);
+        String to = query.get(1);
+
+        if (!map.containsKey(from) || !map.containsKey(to))
+            return -1.0;
+
+        Set<String> seen = new HashSet<>();
+        q.add(new State(from, 1.0));
+
+        while (!q.isEmpty()) {
+            State state = q.poll();
+            if (seen.contains(state.key))
+                continue;
+            if (state.key.equals(to))
+                return state.val;
+            seen.add(state.key);
+            for (Map.Entry<String, Double> e: map.getOrDefault(state.key, new HashMap<>()).entrySet()) {
+                q.add(new State(e.getKey(), state.val * e.getValue()));
+            }
+        }
+
+        return -1.0;
+    }
+
+    public double[] calcEquation2(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, Map<String, Double>> map = new HashMap<>();
+        for (int i = 0; i < equations.size(); ++i) {
+            String num = equations.get(i).get(0);
+            String den = equations.get(i).get(1);
+            map.putIfAbsent(num, new HashMap<>());
+            map.putIfAbsent(den, new HashMap<>());
+            map.get(num).put(den, values[i]);
+            map.get(den).put(num, 1/values[i]);
+        }
+
+        double[] ans = new double[queries.size()];
+        for (int i = 0; i < queries.size(); ++i) {
+            ans[i] = calc(queries.get(i), map);
         }
 
         return ans;
