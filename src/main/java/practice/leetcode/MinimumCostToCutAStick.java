f8c7833524c651c8329b050e0f151d8f2e03af6e
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-12-26 19:30:55 +0530

    Updating code to use List<Integer>

--- src/main/java/practice/leetcode/MinimumCostToCutAStick.java
+++ src/main/java/practice/leetcode/MinimumCostToCutAStick.java
@@ -6,9 +6,8 @@
 
 package practice.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MinimumCostToCutAStick {
 
@@ -24,7 +23,7 @@ public class MinimumCostToCutAStick {
         Goal is to find dp[0][cuts.length-1]
     */
 
-    private int cost(int i, int j, int[] cuts) {
+    private int cost(int i, int j, List<Integer> cuts) {
         if (j - i <= 1)
             return 0;
         if (memo[i][j] != null)
@@ -33,21 +32,18 @@ public class MinimumCostToCutAStick {
         for (int k = i+1; k < j; ++k) {
             memo[i][j] = Math.min(
                     memo[i][j],
-                    cuts[j] - cuts[i] + cost(i, k, cuts) + cost(k, j, cuts)
+                    cuts.get(j) - cuts.get(i) + cost(i, k, cuts) + cost(k, j, cuts)
             );
         }
         return memo[i][j];
     }
 
     public int minCost(int n, int[] cuts) {
-        final int m = cuts.length;
-        memo = new Integer[m+2][m+2];
-        int[] _cuts = new int[m+2];
-        for (int i = 1; i <= m; ++i)
-            _cuts[i] = cuts[i-1];
-        _cuts[m+1] = n;
-        Arrays.sort(_cuts);
-        return cost(0, _cuts.length-1, _cuts);
+        memo = new Integer[102][102];
+        List<Integer> _cuts = new ArrayList<>(Arrays.asList(0, n));
+        Collections.addAll(Arrays.stream(cuts).boxed().collect(Collectors.toList()));
+        Collections.sort(_cuts);
+        return cost(0, _cuts.size()-1, _cuts);
     }
 
 }
