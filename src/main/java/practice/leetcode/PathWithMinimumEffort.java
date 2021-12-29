94af6ccde25ef24eb50e179db26c07e600491b8c
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2021-12-29 19:32:07 +0530

    Updating code to be more "interview-like"

--- src/main/java/practice/leetcode/PathWithMinimumEffort.java
+++ src/main/java/practice/leetcode/PathWithMinimumEffort.java
@@ -11,39 +11,49 @@ import java.util.*;
 
 public class PathWithMinimumEffort {
 
+    private class State {
+        int i, j;
+        int effort; // min effort known in current path
+        State(int i, int j, int effort) {
+            this.i = i;
+            this.j = j;
+            this.effort = effort;
+        }
+    }
+
     public int minimumEffortPath(int[][] heights) {
-        int m = heights.length;
-        int n = heights[0].length;
-        // tracks known min. effort at [i][j]
-        int[][] effort = new int[m][n];
-        for (int[] e: effort)
-            Arrays.fill(e, Integer.MAX_VALUE);
-        effort[0][0] = 0;
-        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e[0]));
-        // tuple of (max. effort in current path, i, j)
-        pq.add(new int[]{0, 0, 0});
+        final int m = heights.length;
+        final int n = heights[0].length;
+        int[][] minEffort = new int[m][n];
+        for (int[] minEff: minEffort)
+            Arrays.fill(minEff, Integer.MAX_VALUE);
+
+        minEffort[0][0] = 0;
+        Queue<State> pq = new PriorityQueue<>(Comparator.comparingInt(s -> s.effort));
+        pq.add(new State(0, 0, 0));
         boolean[][] vis = new boolean[m][n];
-        int[][] pos = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int[][] pos = new int[][]{{-1,0}, {0,1}, {1,0}, {0,-1}};
 
         while (!pq.isEmpty()) {
-            int[] c = pq.poll();
-            if (vis[c[1]][c[2]])
+            State state = pq.poll();
+            if (vis[state.i][state.j])
                 continue;
-            vis[c[1]][c[2]] = true;
+            vis[state.i][state.j] = true;
+
             for (int[] p: pos) {
-                int x = p[0]+c[1];
-                int y = p[1]+c[2];
-                if (x >= 0 && x < m && y >= 0 && y < n) {
-                    // can relax only if max {abs. diff, max. effort in current path} < known min. effort at [x][y]
-                    if (Math.max(effort[c[1]][c[2]], Math.abs(heights[c[1]][c[2]] - heights[x][y])) < effort[x][y]) {
-                        effort[x][y] = Math.max(effort[c[1]][c[2]], Math.abs(heights[c[1]][c[2]] - heights[x][y]));
-                        pq.add(new int[]{effort[x][y], x, y});
+                int ni = state.i + p[0];
+                int nj = state.j + p[1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                    int effort = Math.max(state.effort, Math.abs(heights[state.i][state.j] - heights[ni][nj]));
+                    if (effort < minEffort[ni][nj]) {
+                        minEffort[ni][nj] = effort;
+                        pq.add(new State(ni, nj, effort));
                     }
                 }
             }
         }
 
-        return effort[m-1][n-1];
+        return minEffort[m-1][n-1];
     }
 
     public int minimumEffortPathSpfa(int[][] heights) {
