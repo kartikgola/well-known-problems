a52cc563886bed876760264ee06aa8eeff483615
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-01-02 00:00:44 +0530

    Adding TD approaches

--- src/main/java/practice/leetcode/MinimumDifficultyOfAJobSchedule.java
+++ src/main/java/practice/leetcode/MinimumDifficultyOfAJobSchedule.java
@@ -8,10 +8,8 @@ package practice.leetcode;
 
 public class MinimumDifficultyOfAJobSchedule {
 
-    private Integer[][] dp;
-
     // f(i, j) = min difficulty of job-schedule that starts with ith job on jth day
-    private int f(int[] jobDiff, int i, int j, int d) {
+    private int f(int[] jobDiff, Integer[][] dp, int i, int j, int d) {
         if (j >= d) {
             // if there are still pending jobs, they need to be completed on the last day that is, (d-1)th day
             if (i < jobDiff.length) {
@@ -31,7 +29,7 @@ public class MinimumDifficultyOfAJobSchedule {
 
         for (int k = i; k < jobDiff.length - (d-j) + 1; ++k) {
             diff = Math.max(diff, jobDiff[k]);
-            ans = Math.min(ans, diff + f(jobDiff, k+1, j+1, d));
+            ans = Math.min(ans, diff + f(jobDiff, dp, k+1, j+1, d));
         }
 
         return dp[i][j] = ans;
@@ -40,7 +38,44 @@ public class MinimumDifficultyOfAJobSchedule {
     public int minDifficulty(int[] jobDiff, int d) {
         if (jobDiff.length < d)
             return -1;
-        dp = new Integer[jobDiff.length][d];
-        return f(jobDiff, 0, 0, d);
+        return f(jobDiff, new Integer[jobDiff.length][d], 0, 0, d);
+    }
+
+
+    // Slight optimization of previous approach
+    // Since difficulty of a day is max {jobs_for_that_day}, we pre-compute the hardest jobs from right to left
+    // f(i, j) = min difficulty of job-schedule that starts with ith job on jth day
+    private int f2(int[] jobDiff, int[] hardest, Integer[][] dp, int i, int j, int d) {
+        if (j >= d) {
+            // if there are still pending jobs, they need to be completed on the last day that is, (d-1)th day
+            if (i < jobDiff.length)
+                return hardest[i];
+            return 0;
+        }
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        int diff = jobDiff[i];
+        int ans = Integer.MAX_VALUE;
+
+        for (int k = i; k < jobDiff.length - (d-j) + 1; ++k) {
+            diff = Math.max(diff, jobDiff[k]);
+            ans = Math.min(ans, diff + f2(jobDiff, hardest, dp, k+1, j+1, d));
+        }
+
+        return dp[i][j] = ans;
+    }
+
+    public int minDifficulty2(int[] jobDiff, int d) {
+        if (jobDiff.length < d)
+            return -1;
+        int hardestJob = Integer.MIN_VALUE;
+        int[] hardest = new int[jobDiff.length];
+        for (int i = jobDiff.length-1; i >= 0; --i) {
+            hardestJob = Math.max(hardestJob, jobDiff[i]);
+            hardest[i] = hardestJob;
+        }
+        return f2(jobDiff, hardest, new Integer[jobDiff.length][d], 0, 0, d);
     }
 }
