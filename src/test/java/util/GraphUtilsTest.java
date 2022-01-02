7ec3fc08062b2a3b183ef083c95550215e120914
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-01-02 13:54:25 +0530

    Adding test cases for cut-vertices and cut-edges

--- src/test/java/util/GraphUtilsTest.java
+++ src/test/java/util/GraphUtilsTest.java
@@ -9,11 +9,34 @@ package util;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraphUtilsTest {
 
     @Test
     void topologicalSort() {
+    }
+
+    @Test
+    void cutVerticesTest1() {
+        GraphUtils.IntGraph ig = new GraphUtils.IntGraph(4, false, new int[][]{
+                {0, 1},
+                {1, 2},
+                {2, 0},
+                {1, 3}
+        });
+        assertEquals(Collections.singletonList(1), ig.cutVertices());
+    }
+
+    @Test
+    void cutVerticesTest2() {
+        GraphUtils.IntGraph ig = new GraphUtils.IntGraph(3, false, new int[][]{
+                {0, 1},
+                {0, 2},
+        });
+        assertEquals(Collections.singletonList(0), ig.cutVertices());
     }
 }
