59a71dc147dcb882d034ff818170be74c0a83ff9
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-01-04 18:15:06 +0530

    Updating code in FindFirstAndLastPositionOfElementInSortedArray

--- practice/src/main/java/leetcode/FindFirstAndLastPositionOfElementInSortedArray.java
+++ practice/src/main/java/leetcode/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -2,45 +2,48 @@
  * Author: Kartik Gola
  * Date: 1/2/22, 3:27 PM
  * Copyright (c) 2021 | https://kartikgola.com
+ * Problem URL: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
  */
 
 package leetcode;
 
 public class FindFirstAndLastPositionOfElementInSortedArray {
 
-    private int l(int[] nums, int x, int i, int j) {
-        while (i < j) {
-            int m = i+(j-i)/2;
-            if (nums[m] == x) {
-                int k = l(nums, x, i, m-1);
-                return k == -1 ? m : k;
-            } else if (nums[m] > x) {
-                return l(nums, x, i, m-1);
-            } else {
-                return l(nums, x, m+1, j);
-            }
+    private int findFirst(int[] nums, int x) {
+        int l = 0, r = nums.length-1;
+        while (l < r) {
+            int m = l+(r-l)/2;
+            // p(x) = middle value is greater than equal to x
+            // this makes sure that l is always pointing to the least index that satisfies p(x)
+            if (nums[m] >= x)
+                r = m;
+            else
+                l = m+1;
         }
-        return nums[i] == x ? i : -1;
+        // blank array condition
+        if (l >= nums.length)
+            return -1;
+        return nums[l] == x ? l : -1;
     }
 
-    private int r(int[] nums, int x, int i, int j) {
-        while (i < j) {
-            int m = i+(j-i)/2;
-            if (nums[m] == x) {
-                int k = r(nums, x, m+1, j);
-                return k == -1 ? m : k;
-            } else if (nums[m] > x) {
-                return r(nums, x, i, m-1);
-            } else {
-                return r(nums, x, m+1, j);
-            }
+    private int findLast(int[] nums, int x) {
+        int l = 0, r = nums.length-1;
+        while (l < r) {
+            int m = l+(r-l)/2;
+            // predicate = middle value is greater than equal to x+1
+            // this makes sure that l is always pointing to the least index that satisfies p(x)
+            if (nums[m] >= x+1)
+                r = m;
+            else
+                l = m+1;
         }
-        return nums[i] == x ? i : -1;
+        // blank array condition
+        if (l >= nums.length)
+            return -1;
+        return nums[l] == x ? l : (l-1 >= 0 && nums[l-1] == x ? l-1 : -1);
     }
 
     public int[] searchRange(int[] nums, int x) {
-        if (nums.length == 0)
-            return new int[]{-1, -1};
-        return new int[]{l(nums, x, 0, nums.length-1), r(nums, x, 0, nums.length-1)};
+        return new int[]{findFirst(nums, x), findLast(nums, x)};
     }
 }
