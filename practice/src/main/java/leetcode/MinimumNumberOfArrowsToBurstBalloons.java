1fcfabcbb96840f099d5877e3a5ff370538e927a
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-01-13 20:17:34 +0530

    Adding MinimumNumberOfArrowsToBurstBalloons

--- practice/src/main/java/leetcode/MinimumNumberOfArrowsToBurstBalloons.java
+++ practice/src/main/java/leetcode/MinimumNumberOfArrowsToBurstBalloons.java
@@ -17,16 +17,50 @@ public class MinimumNumberOfArrowsToBurstBalloons {
         Arrays.sort(points, Comparator.comparingInt((int[] a) -> a[0]));
 
         int arrows = 1;
-        int x = points[0][1]; // point at which arrow is shot
+        int max = points[0][1]; // max point at which arrow can be shot at
         for ( int curr = 1; curr < n; ++curr ) {
-            if ( points[curr][0] > x ) { // current start lies outside
-                x = points[curr][1];
+            if ( points[curr][0] > max ) { // current start lies outside
+                max = points[curr][1];
                 ++arrows;
             } else { // current start lies inside
-                x = Math.min(x, points[curr][1]);
+                max = Math.min(max, points[curr][1]);
             }
         }
 
         return arrows;
+    }
+
+    public int findMinArrowShots2(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(p -> p[0]));
+
+        int ans = 1;
+
+        // denotes the inclusive-range in which balloon can be shot
+        int min = points[0][0];
+        int max = points[0][1];
+
+        // [[1,6],[2,8],[7,12],[10,16]]
+        // [1,6] => [1,6]
+        // [1,6], [2,8] => [2,6]
+        // [1,6], [2,8], [7,12] => [2,6], [7,12]
+        // [1,6], [2,8], [7,12], [10,16] => [2,6], [10,12]
+
+        for (int i = 1; i < points.length; ++i) {
+            int cmin = points[i][0];
+            int cmax = points[i][1];
+
+            // if current minimum lies inside the range, update the range
+            if (cmin >= min && cmin <= max) {
+                min = Math.max(min, cmin);
+                max = Math.min(max, cmax);
+            } else {
+                // otherwise, create new range and increment answer
+                ans++;
+                min = cmin;
+                max = cmax;
+            }
+        }
+
+        return ans;
     }
 }
