48df1d76079b74ba1a2ea95045a8c35b0fcca377
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-01-15 23:31:11 +0530

    Adding solution to unstructured deadlock

--- fundamentals/src/main/java/concurrency/liveness/Deadlock.java
+++ fundamentals/src/main/java/concurrency/liveness/Deadlock.java
@@ -81,7 +81,65 @@ public class Deadlock {
         }
     }
 
+    private static void example2Solution() {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        try {
+            Lock lock1 = new ReentrantLock();
+            Lock lock2 = new ReentrantLock();
+
+            // Below code is free from deadlock but not livelock!
+            es.execute(() -> {
+                boolean isDone = false;
+                while (!isDone && !Thread.interrupted()) {
+                    if (lock1.tryLock()) {
+                        if (lock2.tryLock()) {
+                            System.out.println(Thread.currentThread());
+                            isDone = true;
+                            lock2.unlock();
+                            lock1.unlock();
+                        } else {
+                            // couldn't lock2, will try again in loop
+                            // but first, release lock1
+                            lock1.unlock();
+                        }
+                    } else {
+                        // couldn't get lock1, will try again in loop
+                    }
+                }
+            });
+
+            es.execute(() -> {
+                boolean isDone = false;
+                while (!isDone && !Thread.interrupted()) {
+                    if (lock2.tryLock()) {
+                        if (lock1.tryLock()) {
+                            System.out.println(Thread.currentThread());
+                            isDone = true;
+                            lock1.unlock();
+                            lock2.unlock();
+                        } else {
+                            // couldn't lock1, will try again in loop
+                            // but first, release lock2
+                            lock2.unlock();
+                        }
+                    } else {
+                        // couldn't get lock2, will try again in loop
+                    }
+                }
+            });
+
+            es.shutdown();
+            es.awaitTermination(3, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            es.shutdownNow();
+        }
+    }
+
     public static void main(String[] args) {
         example1();
+        example2();
+        example2Solution();
     }
 }
