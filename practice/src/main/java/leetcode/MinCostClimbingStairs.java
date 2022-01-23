1261763ebb0a1a7544eaabd9ccbe3e30e6a683ec
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-01-23 19:45:39 +0530

    Adding bottom-up solution to MinCostClimbingStairs

--- practice/src/main/java/leetcode/MinCostClimbingStairs.java
+++ practice/src/main/java/leetcode/MinCostClimbingStairs.java
@@ -23,4 +23,15 @@ public class MinCostClimbingStairs {
         final Integer[] dp = new Integer[cost.length];
         return Math.min(climb(cost, dp, 0), climb(cost, dp, 1));
     }
+
+    public int minCostClimbingStairs2(int[] cost) {
+        final int n = cost.length;
+        int[] dp = new int[n+2];
+
+        for (int i = n-1; i >= 0; --i) {
+            dp[i] = Math.min(Integer.MAX_VALUE, cost[i] + Math.min(dp[i+1], dp[i+2]));
+        }
+
+        return Math.min(dp[0], dp[1]);
+    }
 }
