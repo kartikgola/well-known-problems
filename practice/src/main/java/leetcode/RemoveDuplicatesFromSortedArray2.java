a8a3aaa6dc6f8521a921d71d8e7dd02d214f491c
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2022-02-06 23:54:01 +0530

    Adding RemoveDuplicatesFromSortedArray2

--- practice/src/main/java/leetcode/RemoveDuplicatesFromSortedArray2.java
+++ practice/src/main/java/leetcode/RemoveDuplicatesFromSortedArray2.java
@@ -9,29 +9,20 @@ package leetcode;
 public class RemoveDuplicatesFromSortedArray2 {
 
     public int removeDuplicates(int[] nums) {
-        final int n = nums.length;
-        final int k = 2; // max permissible repetitions
-        if (n <= k) return n;
+        int j = 1; // write head
+        int prev = 0; // previous number to compare current number with
 
-        int count = 1; // stores current count of occurances
-        int prev = 0;  // stores previous write index
-
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] != nums[i - 1]) {
-                // fill up min(count, k) positions with previous value
-                for (int j = prev; j < prev + Math.min(count, k); ++j)
-                    nums[j] = nums[i - 1];
-                // increase prev and reset count
-                prev += Math.min(count, k);
-                count = 1;
-            } else ++count;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == nums[prev]) {
+                if (i-prev < 2) {
+                    nums[j++] = nums[i];
+                } // else do nothing
+            } else {
+                nums[j++] = nums[i];
+                prev = i;
+            }
         }
 
-        // At this point, it can be [x x x y] or [x x x y y] or [x y z] or [x y y]
-        for (int j = prev; j < prev + Math.min(count, k); ++j)
-            nums[j] = nums[n - 1];
-        prev += Math.min(count, k);
-
-        return prev;
+        return j;
     }
 }
